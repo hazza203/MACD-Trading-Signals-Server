3e3cac2f1d8192888a05b64ac526bd86c82d6e43
Hazza203 <s3383212@student.rmit.edu.au>
2019-04-18 15:57:27 +0800

    first commit to heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+mongoose.connect('', {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to db'))
+
+app.get('/', (req, res) => {res.send('We are working')})
+/*
+setInterval(() => {
+	fetch('https://api.binance.com/api/v1/ticker/24hr?symbol=NEOBTC')
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+	})
+},10000)*/
